0c727402473e3870c08c9bcfa7f0d66991023cc2
Harrison Taylor <h.taylor@project89.org>
2015-04-23 15:46:34 -0400

    Added controllers, index runs

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,33 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_url
+  end
+
+    private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :city, :adress, :cuisine, :email, :phone, :seats, :opening_time, :closing_time)
+  end
+end
